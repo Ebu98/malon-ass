c28d1d76b832435358a7607fa15c84e2545d9442
Ebu98 <91473642+Ebu98@users.noreply.github.com>
2023-06-23 10:04:24 +0100

    changes made

--- src/context/post.js
+++ src/context/post.js
@@ -108,5 +108,4 @@ export default PostProvider;
         ))}
         </select> */}
         // https://www.figma.com/file/aNrYJS9BEwLmPU5z5ddPXZ/Login-%26-Signup-Screen-for-Web-(Community)?type=design&node-id=0-1&t=w8JnJGMNkR4Gs3y1-0
-
-        https://meet.google.com/yex-nbrb-gqp
+        https://meet.google.com/btr-thfn-cpo
